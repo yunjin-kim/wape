8926c3b075b69989415c15987dd26d99e2198e47
(yunjin) <sion11290805@gmail.com>
2021-07-27 16:41:34 +0900

    myloc

--- mainpage_js.js/mappage.js
+++ mainpage_js.js/mappage.js
@@ -1,6 +1,5 @@
 const array = [];
-const array1 = [];
-//드래그 해서 중심 좌표 변경할 수 있게 map을 어떻게 쓸수 있게
+// 드래그 해서 중심 좌표 변경할 수 있게 map을 어떻게 쓸수 있게
 // let level1;
 // let latlng1;
 // kakao.maps.event.addListener(map, 'center_change', function(){
@@ -9,16 +8,17 @@ const array1 = [];
 // })
 // console.log(level1)
 // console.log(latlng1)
+let mapOption;
+let map;
 
 function getLocation(){
   navigator.geolocation.getCurrentPosition(getGeo);
 }
-
 function getGeo(event) {
   const lat = event.coords.latitude;
   const lon = event.coords.longitude;
 
-  const mapOption = {
+  mapOption = {
     center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
     level: 2 // 지도의 확대 레벨
   };
@@ -28,12 +28,12 @@ function getGeo(event) {
 const $showMap = document.getElementById('showmap');
 const $mapText = document.querySelector('.mappage__location__text ');
 const geocoder = new kakao.maps.services.Geocoder();// 주소-좌표 변환 객체를 생성합니다
+console.log(geocoder)
 const pointArr = [];
 
 async function getMap(mapOption) {
 
-  // 지도를 생성합니다    
-  let map;
+  // 지도를 생성합니다
   if(array.length < 1){
     map = new kakao.maps.Map($showMap, mapOption);
     array.push(map);
@@ -49,11 +49,10 @@ async function getMap(mapOption) {
   }
   console.log(array);
   console.log(array.length)
-  
-
 
   // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
   searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+
 
   // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
   kakao.maps.event.addListener(map, 'idle', function () {
@@ -77,25 +76,58 @@ async function getMap(mapOption) {
   };
   
   showMarker(map, pointArr);
-  myLocation(map, mapOption);
-}
-// navigator.geolocation.watchPosition(myLocation)
 
-//내 위치 표시   map 계속 생기는 문제 발생
-function myLocation(map, mapOption){
-      console.log("내 위치 탐색");
-      const locImg = '../img/mylocation.png';
-      const myLocSize = new kakao.maps.Size(14,14);
-      const myLocImg = new kakao.maps.MarkerImage(locImg, myLocSize);
-      let myLoc = new kakao.maps.LatLng(mapOption.center.Ma, mapOption.center.La);
-      console.log(mapOption.center.Ma, mapOption.center.La);
-      let mypos = new kakao.maps.Marker({
-        map: map,
-        position: myLoc,
-        image: myLocImg,
-      })
-      mypos.setMap(map);
 }
+
+setTimeout(()=>{
+  navigator.geolocation.watchPosition(getMyLocation);
+
+  function getMyLocation(){
+    navigator.geolocation.getCurrentPosition(getMyGeo);
+  }
+  function getMyGeo(event) {
+    const lat1 = event.coords.latitude;
+    const lon1 = event.coords.longitude;
+    console.log(lat1, lon1)
+  
+    myMapOption = {
+      center: new kakao.maps.LatLng(lat1, lon1), // 지도의 중심좌표
+      level: 2 // 지도의 확대 레벨
+    };
+    // myLocation(myMapOption);
+    gps_tracking(myMapOption);
+  };
+},4000)
+
+
+//내 위치 표시  
+// function myLocation(myMapOption){
+//       console.log("내 위치 탐색");
+//       const locImg = '../img/mylocation.png';
+//       const myLocSize = new kakao.maps.Size(14,14);
+//       const myLocImg = new kakao.maps.MarkerImage(locImg, myLocSize);
+//       let myLoc = new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La);
+//       console.log(myMapOption.center.Ma, myMapOption.center.La);
+//       let mypos = new kakao.maps.Marker({
+//         map: map,
+//         position: myLoc,
+//         image: myLocImg,
+//       })
+//       mypos.setMap(map);
+// }
+
+function gps_tracking(myMapOption){
+  console.log('위치 최신화')
+      map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
+      const gps_content = '<img src="../img/mylocation.png" />';
+      var currentOverlay = new kakao.maps.CustomOverlay({
+          map: map,
+          position: new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La),
+          content: gps_content,
+      });
+      currentOverlay.setMap(map);
+}
+
 
 if(navigator.geolocation){
   getLocation();
