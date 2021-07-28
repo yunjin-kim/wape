d222eac5290ad492b6a37c60e4bd696407c71932
(yunjin) <sion11290805@gmail.com>
2021-07-28 16:18:48 +0900

    map

--- mainpage_js.js/mappage.js
+++ mainpage_js.js/mappage.js
@@ -1,15 +1,6 @@
-const array = [];
-// 드래그 해서 중심 좌표 변경할 수 있게 map을 어떻게 쓸수 있게
-// let level1;
-// let latlng1;
-// kakao.maps.event.addListener(map, 'center_change', function(){
-//   level1  = map.getLevel();
-//   latlng1 = map.getCenter();
-// })
-// console.log(level1)
-// console.log(latlng1)
 let mapOption;
 let map;
+let dragged = false;
 
 function getLocation(){
   navigator.geolocation.getCurrentPosition(getGeo);
@@ -34,25 +25,10 @@ const pointArr = [];
 async function getMap(mapOption) {
 
   // 지도를 생성합니다
-  if(array.length < 1){
     map = new kakao.maps.Map($showMap, mapOption);
-    array.push(map);
-  }
-  else if(array.length >= 1){
-    // mapOption = {
-    //   center: new kakao.maps.LatLng(latlng1), // 지도의 중심좌표
-    //   level:level1 // 지도의 확대 레벨
-    // };
-    map = new kakao.maps.Map($showMap, mapOption);
-    array.push(map);
-    array.shift();
-  }
-  console.log(array);
-  console.log(array.length)
 
   // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
   searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-
 
   // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
   kakao.maps.event.addListener(map, 'idle', function () {
@@ -75,11 +51,12 @@ async function getMap(mapOption) {
     pointArr.push(points);
   };
   
-  showMarker(map, pointArr);
+  showMarker(pointArr);
 
 }
 
 setTimeout(()=>{
+
   navigator.geolocation.watchPosition(getMyLocation);
 
   function getMyLocation(){
@@ -96,36 +73,50 @@ setTimeout(()=>{
     };
     // myLocation(myMapOption);
     gps_tracking(myMapOption);
+
   };
 },4000)
 
+const $nowLocation = document.querySelector('.mappage__location__btn');
 
-//내 위치 표시  
-// function myLocation(myMapOption){
-//       console.log("내 위치 탐색");
-//       const locImg = '../img/mylocation.png';
-//       const myLocSize = new kakao.maps.Size(14,14);
-//       const myLocImg = new kakao.maps.MarkerImage(locImg, myLocSize);
-//       let myLoc = new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La);
-//       console.log(myMapOption.center.Ma, myMapOption.center.La);
-//       let mypos = new kakao.maps.Marker({
-//         map: map,
-//         position: myLoc,
-//         image: myLocImg,
-//       })
-//       mypos.setMap(map);
-// }
+//클릭하면 dragged 가 false로 바뀌고 현재 위치로 바로 갈 수 있게
+$nowLocation.addEventListener('click', ()=>{
+  dragged = false;
+  console.log(dragged)
+})
 
+const arr1 = [];
 function gps_tracking(myMapOption){
-  console.log('위치 최신화')
+  console.log(dragged)
+  let currentOverlay;
+  const gps_content = '<img src="../img/mylocation.png" />';
+  
+  if(arr1.length === 0){
+    console.log('위치 최신화')
+
+    if(dragged === false){
+      console.log('aaa')
       map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
-      const gps_content = '<img src="../img/mylocation.png" />';
-      var currentOverlay = new kakao.maps.CustomOverlay({
-          map: map,
-          position: new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La),
-          content: gps_content,
-      });
-      currentOverlay.setMap(map);
+    }
+
+    currentOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La),
+        content: gps_content,
+    });
+    arr1.push(currentOverlay);
+  }
+  else{
+    currentOverlay = new kakao.maps.CustomOverlay({
+      map: map,
+      position: new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La),
+      content: gps_content,
+    });
+    arr1.push(currentOverlay);
+
+  }
+  console.log(arr1)
+  currentOverlay.setMap(map);
 }
 
 
@@ -143,9 +134,10 @@ const $courseName = document.querySelector('.mappage__walkload__course__name');
 const $courseLocation = document.querySelector('.mappage__walkload__course__location');
 const $courseDistance = document.querySelector('.mappage__walkload__course__distance');
 const $courseMoney = document.querySelector('.mappage__walkload__course__money');
+
 //걷기 포인트 표시
-function showMarker(map, pointArr) {
-  const imgSrc = '../img/loc.png';
+function showMarker(pointArr) {
+  const imgSrc = '../img/loaction.png';
   const imgSize = new kakao.maps.Size(18, 26);
   const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
   let markerArr = [];
@@ -172,11 +164,12 @@ function showMarker(map, pointArr) {
     })
   }
 
-
   //드래그로 지도 이동을 완료했을 때 마지막 파라미터로 넘어온 함수를 호출
   kakao.maps.event.addListener(map, 'dragend', function () {
-    let mapLevel = map.getLevel();
+    dragged = true;
+    console.log(dragged)
     let mapCenter = map.getCenter();
+    let mapLevel = map.getLevel();
     let shortDistance = Number.MAX_SAFE_INTEGER;
     let nearMark;
     for (let i = 0; i < pointArr[0].length; i++) {
@@ -192,7 +185,6 @@ function showMarker(map, pointArr) {
     $courseDistance.textContent = nearMark.distance;
     $courseMoney.textContent = nearMark.money;
 
-    return mapLevel;
   })
 }
 
