efacbd248d54ad220cf534eca96ca1038eedc7ef
(yunjin) <sion11290805@gmail.com>
2021-08-01 15:56:27 +0900

    map

--- mainpage_js.js/mappage.js
+++ mainpage_js.js/mappage.js
@@ -2,16 +2,16 @@ let mapOption;
 let map;
 let dragged = false;
 
-if(navigator.geolocation){
+if (navigator.geolocation) {
   getLocation();
   // setInterval(getLocation, 3000);
 }
-else{
+else {
   console.log('error');
 }
 
 
-function getLocation(){
+function getLocation() {
   navigator.geolocation.getCurrentPosition(getGeo);
 }
 function getGeo(event) {
@@ -34,7 +34,7 @@ const pointArr = [];
 async function getMap(mapOption) {
 
   // 지도를 생성합니다
-    map = new kakao.maps.Map($showMap, mapOption);
+  map = new kakao.maps.Map($showMap, mapOption);
 
   // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
   searchAddrFromCoords(map.getCenter(), displayCenterInfo);
@@ -59,87 +59,74 @@ async function getMap(mapOption) {
   function getData(points) {
     pointArr.push(points);
   };
-  
+
   showMarker(pointArr);
 
 }
 
 const myLocArr = [];
-setTimeout(()=>{
+setTimeout(() => {
+  showWalkBanner();
   //내 위치가 변경되면 좌표를 반환한다. map을 재생성하는 것을 방지
   // navigator.geolocation.watchPosition(getMyLocation);
   //watchPosition로 하면 너무 부정확해서 setinerval로 보류
-setInterval(getMyLocation, 4000);
-  function getMyLocation(){
+  setInterval(getMyLocation, 4000);
+  function getMyLocation() {
     navigator.geolocation.getCurrentPosition(getMyGeo);
   }
   function getMyGeo(event) {
     const lat1 = event.coords.latitude;
     const lon1 = event.coords.longitude;
     console.log(lat1, lon1)
-  
+
     myMapOption = {
       center: new kakao.maps.LatLng(lat1, lon1), // 지도의 중심좌표
       level: 2 // 지도의 확대 레벨
     };
     showMyLoc(myMapOption);
   };
-},4000)
+}, 4000)
 
 const $backToMyLoc = document.querySelector('.mappage__location__btn');
 
 //클릭하면 dragged 가 false로 바뀌고 현재 위치로 바로 갈 수 있게
-$backToMyLoc.addEventListener('click', ()=>{
+$backToMyLoc.addEventListener('click', () => {
   dragged = false;
   map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
 })
 
 //내 위치 표시
 let currentMyLoc;
-function showMyLoc(myMapOption){
+function showMyLoc(myMapOption) {
   console.log(dragged);
   console.log(myLocArr)
   const myLocIconImg = '../img/mylocation.png';
-  const myLocIconSize = new kakao.maps.Size(10,10);
+  const myLocIconSize = new kakao.maps.Size(10, 10);
   const myLocIcon = new kakao.maps.MarkerImage(myLocIconImg, myLocIconSize);
   let myLocPosition = new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La);
-   
-    if(dragged === false){
-      console.log('aaa')
-      map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
-    }
 
-    if(myLocArr.length <= 0){
-      console.log('aa')
-        currentMyLoc = new kakao.maps.Marker({
-        map: map,
-        position: myLocPosition,
-        image: myLocIcon,
-      });
-      myLocArr.push(currentMyLoc);
-      currentMyLoc.setMap(map);
-    }
-    else if(myLocArr.length > 0){
-      currentMyLoc.setPosition(myLocPosition);
-      console.log('bb')
-    }
+  if (dragged === false) {
+    console.log('aaa')
+    map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
+  }
+
+  if (myLocArr.length <= 0) {
+    console.log('aa')
+    currentMyLoc = new kakao.maps.Marker({
+      map: map,
+      position: myLocPosition,
+      image: myLocIcon,
+    });
+    myLocArr.push(currentMyLoc);
+    currentMyLoc.setMap(map);
+  }
+  else if (myLocArr.length > 0) {
+    currentMyLoc.setPosition(myLocPosition);
+    console.log('bb')
+  }
 }
 
-// function displayMyLoc(myLocPosition){
-//   var myMarker = new kakao.maps.Marker({
-//     map: map,
-//     position: myLocPosition
-//   })
-//   myMarker.setMap(map);
-// }
-
-
-// function controlMyLoc(myMapOption){
-//   currentOverlay.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
-// }
-
-
-
+const $selectCourse = document.querySelector('.mappage__walkload__course');
 const $courseImage = document.querySelector('.mappage__walkload__course__img__img');
 const $courseName = document.querySelector('.mappage__walkload__course__name');
 const $courseLocation = document.querySelector('.mappage__walkload__course__location');
@@ -153,7 +140,7 @@ function showMarker(pointArr) {
   const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
   let markerArr = [];
   let marker;
-
+  //걷기 포인트 표시
   for (let i = 0; i < pointArr[0].length; i++) {
     let markerPos = new kakao.maps.LatLng(pointArr[0][i].lat, pointArr[0][i].lon);
 
@@ -164,7 +151,7 @@ function showMarker(pointArr) {
     markerArr.push(marker);
     marker.setMap(map);
   }
-
+  //배너에 정보 표시
   for (let i = 0; i < markerArr.length; i++) {
     markerArr[i].addListener('click', () => {
       $courseImage.setAttribute("src", pointArr[0][i].image)
@@ -174,29 +161,36 @@ function showMarker(pointArr) {
       $courseMoney.textContent = pointArr[0][i].money;
     })
   }
-  
-  //드래그로 지도 이동을 완료했을 때 마지막 파라미터로 넘어온 함수를 호출
-  kakao.maps.event.addListener(map, 'dragend', function () {
-    dragged = true;
-    console.log(dragged)
-    let mapCenter = map.getCenter();
-    qqq = mapCenter.Ma;
-    let mapLevel = map.getLevel();
-    let shortDistance = Number.MAX_SAFE_INTEGER;
-    let nearMark;
-    for (let i = 0; i < pointArr[0].length; i++) {
-      let markToMark = Math.abs((Number(pointArr[0][i].lat) + Number(pointArr[0][i].lon)) - (mapCenter.Ma + mapCenter.La));
-      if (markToMark < shortDistance) {
-        shortDistance = markToMark;
-        nearMark = pointArr[0][i];
-      }
-    }
-    $courseImage.setAttribute("src", nearMark.image)
-    $courseName.textContent = nearMark.name;
-    $courseLocation.textContent = nearMark.address;
-    $courseDistance.textContent = nearMark.distance;
-    $courseMoney.textContent = nearMark.money;
+
+  //걷기 코스 보여주기
+  $selectCourse.addListener('click', () => {
+
   })
+
+
+  //드래그로 지도 이동을 완료했을 때 마지막 파라미터로 넘어온 함수를 호출
+  kakao.maps.event.addListener(map, 'dragend', showWalkBanner)
+}
+
+function showWalkBanner() {
+  dragged = true;
+  console.log(dragged)
+  let mapCenter = map.getCenter();
+  let mapLevel = map.getLevel();
+  let shortDistance = Number.MAX_SAFE_INTEGER;
+  let nearMark;
+  for (let i = 0; i < pointArr[0].length; i++) {
+    let markToMark = Math.abs((Number(pointArr[0][i].lat) + Number(pointArr[0][i].lon)) - (mapCenter.Ma + mapCenter.La));
+    if (markToMark < shortDistance) {
+      shortDistance = markToMark;
+      nearMark = pointArr[0][i];
+    }
+  }
+  $courseImage.setAttribute("src", nearMark.image)
+  $courseName.textContent = nearMark.name;
+  $courseLocation.textContent = nearMark.address;
+  $courseDistance.textContent = nearMark.distance;
+  $courseMoney.textContent = nearMark.money;
 }
 
 
