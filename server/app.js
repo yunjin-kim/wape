60a2eaeeef96c2834abfdbc97d53c97c4c50f57e
(yunjin) <sion11290805@gmail.com>
2021-08-14 09:39:24 +0900

    server

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// app.set('port', process.env.PORT || 4000);
-// app.get('/', (req,res)=>{
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
+app.set('port', process.env.PORT || 4000);
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// app.post('/', (req, res)=>{
-//   res.send('hola');
-// })
+app.post('/', (req, res)=>{
+  res.send('hola');
+})
 
-// app.listen(app.get('port'), ()=>{
-//   console.log('익스프레스 서버 실행')
-// });
+app.get('/about',(req, res)=>{
+  res.sendFile(path.join(__dirname, '../onboarding_html/onboarding_influ.html'));
+})
+
+app.listen(app.get('port'), ()=>{
+  console.log('익스프레스 서버 실행')
+});
 
