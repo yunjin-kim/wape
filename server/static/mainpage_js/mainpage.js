4490108c6936039e0c1fc54f61799d3ac09ddb79
(yunjin) <sion11290805@gmail.com>
2021-10-07 20:46:28 +0900

    걷기암림 기능

--- server/static/mainpage_js/mainpage.js
+++ server/static/mainpage_js/mainpage.js
@@ -1,14 +1,17 @@
 import { quoteSentence } from "./mainpage_quote.js";
 import { getCookie } from "./mainpage_profile.js";
 import { renderCalendar, thisYear, thisMonth, daysArray } from "./mainpage_calendar.js";
-import { holeDayArr, holeDateArr } from "./mainpage_reserve.js";
+import { holeDayArr, holeDateArr, clickDate, hourArr, minuteArr } from "./mainpage_reserve.js";
 
+//걷기 효능
 const $quote = document.querySelector('.quote');
 $quote.innerText = quoteSentence;
 
+//프로필
 const $profileName = document.getElementById('profileName');
 $profileName.innerText = getCookie();
 
+//오늘 날씨
 const $atmosCon = document.querySelector('.mainpage__weather__weather');
 const $temp = document.querySelector('.mainpage__weather__temp');
 const $hightemp = document.querySelector('.mainpage__weather__hightemp');
@@ -34,6 +37,7 @@ async function loadWeather(){
 loadWeather()
 
 //달력 
+//1일 되면 다음달로 넘어가는지 확인 필요
 const $thisYearMonth = document.querySelector('.thisYearMonth');
 const $calendarDays = document.querySelector('.mainpage__calendar__day');
 $thisYearMonth.textContent = `${thisYear}.${thisMonth+1}`;
@@ -41,19 +45,33 @@ $calendarDays.innerHTML = daysArray.join(' ');
 
 renderCalendar()
 
-//이번달의 마지막날도 불러와서 예약할 날짜가 마지막 날짜를 넘어가면 1일로 바뀔 수 있게
-// const date = new Date();
-// let todoyDay = date.getDay()
-// const thisLast = new Date(thisYear, thisMonth+1, 0)
-// console.log(date.getDate())
-// console.log(date.getDay())
-// console.log(thisLast.getDate())
-
+//걷기 알림
+//이번달의 마지막날도 불러와서 예약할 날짜가 마지막 날짜를 넘어가면 1일로 바뀔 수 있게 되는지 확인 필요
 const $bookDate = document.querySelector('.mainpage__book__date');
-
+const DATE_SPAN = 'dateSpan';
+const COLORED_BOX = 'coloredBox';
 const bookDays = $bookDate.children;
-console.log(holeDateArr)
+
 for(let i = 0; i < 7; i++){
   bookDays[i].children[0].textContent = holeDayArr[i];
   bookDays[i].children[1].textContent = holeDateArr[i];
+  bookDays[i].children[0].classList.add(DATE_SPAN);
+  bookDays[i].children[1].classList.add(DATE_SPAN);
+  bookDays[i].classList.add(COLORED_BOX)
 }
+
+$bookDate.addEventListener('click', (e)=>{
+  clickDate(e)
+})
+
+
+const $selectHour = document.querySelector(".selectHour");
+for(let i = 0; i < hourArr.length; i++){
+  $selectHour.append(hourArr[i])
+}
+
+const $selectMinute = document.querySelector(".selectMinute");
+for(let i = 0; i < minuteArr.length; i++){
+  $selectMinute.append(minuteArr[i])
+}
+
--- server/static/mainpage_js/mainpage_reserve.js
+++ server/static/mainpage_js/mainpage_reserve.js
@@ -22,3 +22,49 @@ for(let i = 0; i < 7; i++){
   todayDate++;
 }
 
+
+const CLICK_GREEN = 'backgroundGreen';
+const DATE_SPAN = 'dateSpan';
+const COLORED_BOX = 'coloredBox';
+
+export function clickDate(e){
+  if(e.target.classList.contains(COLORED_BOX)){
+
+    if(e.target.classList.contains(CLICK_GREEN)){
+      e.target.classList.remove(CLICK_GREEN)
+    }
+    else{
+      e.target.classList.add(CLICK_GREEN)
+    }
+  }
+
+  else if(e.target.classList.contains(DATE_SPAN)){
+    
+    if(e.target.parentNode.classList.contains(CLICK_GREEN)){
+      e.target.parentNode.classList.remove(CLICK_GREEN)
+    }
+    else{
+      e.target.parentNode.classList.add(CLICK_GREEN)
+    }
+  }
+}
+
+  export const hourArr = [];
+  for(let i = 1; i < 24; i++){
+    if(i < 10){
+      i = "0"+i;
+    }
+    let option = document.createElement('option');
+    option.textContent = i;
+    hourArr.push(option);
+  }
+
+  export const minuteArr = [];
+  for(let i = 1; i < 60; i++){
+    if(i < 10){
+      i = "0"+i;
+    }
+    let option = document.createElement('option');
+    option.textContent = i;
+    minuteArr.push(option);
+  }
