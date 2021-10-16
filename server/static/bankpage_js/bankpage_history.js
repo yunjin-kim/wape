643163a3bd4e670db9a625f01be4709323d080bd
(yunjin) <sion11290805@gmail.com>
2021-10-16 21:19:07 +0900

    은행페이지 돈 데이터

--- server/static/bankpage_js/bankpage_history.js
+++ server/static/bankpage_js/bankpage_history.js
@@ -1,5 +1,6 @@
 import { _go, _map } from '../fx.js'
-
+//1걸음 = 70cm
+//한걸음 = 5원
 export function setStepHistory(){
 
   let getStepData = localStorage.getItem("STEP_DATA");
@@ -19,6 +20,11 @@ export function setStepHistory(){
   let myGoal = localStorage.getItem("STEP_GOAL");
 
   for(let i = 0; i < stepValue.length; i++){
+    let myStepGragh = stepValue[i]/myGoal*262;
+    if (myStepGragh > 262){
+      myStepGragh = 262;
+    }
+
     let showHistory = `
       <div class="bankpage__reposit__wrap">
         <div class="bankpage__reposit__title">
@@ -30,7 +36,7 @@ export function setStepHistory(){
             </div>
             <div class="bankpage__reposit__value">
                 <h3>
-                    <span></span>km 걷고<br>
+                    <span>${(daysValue[i]*0.7).toFixed(1)}</span>km 걷고<br>
                     <span></span>원 벌었어요
                 </h3>
             </div>
@@ -42,17 +48,17 @@ export function setStepHistory(){
             </div>
             <div class="bankpage__reposit__walk__graph">
                 <div class="bankpage__reposit__walk__total"></div>
-                <div class="bankpage__reposit__walk__my" style="width: ${stepValue[i]/myGoal*100}px"></div>
+                <div class="bankpage__reposit__walk__my" style="width: ${myStepGragh}px"></div>
             </div>
         </div>
         <div class="bankpage__reposit__money">
             <div class="bankpage__reposit__money__flex">
-                <h4><b>금액 </b><span></span> 원</h4>
-                <p><span></span>원</p>
+                <h4><b>금액 </b><span>${stepValue[i]*5}</span> 원</h4>
+                <p><span>${myGoal*5}</span>원</p>
             </div>
             <div class="bankpage__reposit__money__graph">
                 <div class="bankpage__reposit__money__total"></div>
-                <div class="bankpage__reposit__money__my"></div>
+                <div class="bankpage__reposit__money__my" style="width: ${myStepGragh}px"></div>
             </div>
         </div>
       </div>
