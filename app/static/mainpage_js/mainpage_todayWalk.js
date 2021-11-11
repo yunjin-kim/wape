a70662c80d800cd1ad91387fac52b356200a8e92
(yunjin) <sion11290805@gmail.com>
2021-11-11 15:17:23 +0900

    오늘의걷기기svg

--- app/static/mainpage_js/mainpage_todayWalk.js
+++ app/static/mainpage_js/mainpage_todayWalk.js
@@ -23,8 +23,17 @@ export async function getTodayStepData() {
 
 function showTodayStepData(stepData) {
   const $todayWalkWrap = document.querySelector(".mainpage__today__main");
+  const $todayWalkLine = document.getElementById("mainpage__today__line");
+
+  const walkLine = $todayWalkLine.getContext("2d");
+  walkLine.beginPath();
+  walkLine.lineWidth = 1;
+  walkLine.strokeStyle = "gray";
+  walkLine.moveTo(2,100);
+
   console.log(stepData)
   $todayWalkWrap.style.width = `${stepData.length*90}px`;
+
   for(let i = 0; i < stepData.length; i++) {
     const stepDataDott = document.createElement('div');
     const dottStepText = document.createElement('span')
@@ -43,12 +52,16 @@ function showTodayStepData(stepData) {
       dottStepText.style.top = `${86}px`;
       dottStartTimeText.style.top = `${110}px`;
       dottEndTimeText.style.top = `${120}px`;
+      if(i > 0) {
+        walkLine.bezierCurveTo(i*60, i*30, i*60, i*60, i*90, 105);
+      }
     } 
     else if(i%2 === 1) {
       stepDataDott.style.top = `${50}px`;
       dottStepText.style.top = `${40}px`;
       dottStartTimeText.style.top = `${60}px`;
       dottEndTimeText.style.top = `${70}px`;
+      walkLine.quadraticCurveTo(i*90, i*20, i*90, 55);
     } 
     stepDataDott.style.left = `${i*90}px`;
     dottStepText.style.left = `${i*90}px`;
@@ -56,9 +69,8 @@ function showTodayStepData(stepData) {
     dottEndTimeText.style.left = `${i*90}px`;
     $noTodayStepDataText.append(stepDataDott, dottStepText, dottStartTimeText, dottEndTimeText);
   }
+  walkLine.stroke();
 }
-
-console.log(date.getMonth())
 
 export function showTodayWalkDate() {
   const $todayWalkDate = document.querySelector(".mainpage__today__title__date");
