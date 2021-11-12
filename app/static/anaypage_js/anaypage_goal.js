3fd4338094b887e4e0b0c27b7276c489703e01f0
(yunjin) <sion11290805@gmail.com>
2021-11-12 20:30:22 +0900

    목표달성라펙터링

--- app/static/anaypage_js/anaypage_goal.js
+++ app/static/anaypage_js/anaypage_goal.js
@@ -1,4 +1,5 @@
 import { chartDataArr } from './anaypage_step.js';
+import { _map, _go, _take } from '../fx.js';
 
 //목표 달성 유무
 export function setGoalAchieve(goalBoxArr, goalWeekNum) {
@@ -6,18 +7,36 @@ export function setGoalAchieve(goalBoxArr, goalWeekNum) {
   const noGoalText = `<div class="noGoalText">목표를 설정해주세요</div>`;
 
   if(!goalWeekNum) goalWeekNum = 0;
+  
   if(goalStep) {
+
     for(let i = 0; i < goalBoxArr.length; i++){
       goalBoxArr[i].classList.remove("acheiveGoal", "NotAcheiveGoal");
     }
+
+    // _go(
+    //   _map(goalBox => goalBox.classList.remove("acheiveGoal", "NotAcheiveGoal")),
+    //   _take(6),
+    // goalBoxArr);
+
+    // (_map((chartData, goalBox) => chartData.value >= goalStep ? goalBox.classList.add("acheiveGoal") : goalBox.classList.add("NotAcheiveGoal"),
+    // chartDataArr[goalWeekNum], 
+    // goalBoxArr))
+
+    // (
+    // _map(),
+    // chartDataArr[goalWeekNum],
+    // goalBoxArr)
+
+    console.log(goalBoxArr)
+
     for(let i = 0; i < goalBoxArr.length; i++) {
-      if(chartDataArr[goalWeekNum][i].value >= goalStep){
-        goalBoxArr[i].classList.add("acheiveGoal")
-      }
-      else if(chartDataArr[goalWeekNum][i].value < goalStep) {
-        goalBoxArr[i].classList.add("NotAcheiveGoal")
-      }
+      {chartDataArr[goalWeekNum][i].value >= goalStep
+        ? goalBoxArr[i].classList.add("acheiveGoal")
+        :goalBoxArr[i].classList.add("NotAcheiveGoal")}
     }
+
+
   }
   else {
     return noGoalText;
