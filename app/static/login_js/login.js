590749240500636be6806c26d51e9d83b009fd11
(yunjin) <sion11290805@gmail.com>
2021-11-20 18:14:21 +0900

    로그인함수변경

--- app/static/login_js/login.js
+++ app/static/login_js/login.js
@@ -1,18 +1,18 @@
 let loginNum;
-function loginNumber(){
-  loginNum = document.getElementById('loginNum').value;
-  console.log(loginNum);
-}
+const $loginNumInput = document.querySelector(".loginNum");
+$loginNumInput.addEventListener('change', (e) => {
+  loginNum = e.target.value;
+})
 
 let loginPass;
-function loginPassword(){
-  loginPass = document.getElementById('loginPass').value;
-  console.log(loginPass)
-}
+const $loginPassInput = document.querySelector(".loginPass");
+$loginPassInput.addEventListener('change', (e) => {
+  loginPass = e.target.value;
+})
 
 const $loginBtn = document.getElementById("loginBtn");
 
-$loginBtn.addEventListener('click', ()=>{
+$loginBtn.addEventListener('click', () => {
   console.log("로그인111")
   if(loginNum);
   if(loginPass);
@@ -22,14 +22,14 @@ $loginBtn.addEventListener('click', ()=>{
 
 const $loginError = document.getElementById('loginError');
 
-function postLoginInfo(){
-  axios.post('/auth/login', {number: loginNum,password: loginPass})
-    .then(function(res){
-      if(res.data.redirect == '/page/main'){
+function postLoginInfo() {
+  axios.post('/auth/login', {number: loginNum, password: loginPass})
+    .then(function(res) {
+      if(res.data.redirect == '/page/main') {
         window.location = '/page/main'
       }
-    }).then(()=>{
-  }).catch(error =>{
+    })
+    .catch(error  => {
     console.log(error);
     $loginError.textContent = "입력한 정보가 올바르지 않습니다";
   })
