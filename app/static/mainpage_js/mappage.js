f5fbf40433acdc970948b9e430f9aaa7e91f8ca7
(yunjin) <sion11290805@gmail.com>
2021-11-27 20:53:11 +0900

    feat: 걷기 코스 레이즈로드 적용

--- app/static/mainpage_js/mappage.js
+++ app/static/mainpage_js/mappage.js
@@ -3,12 +3,11 @@ let dragged = false;
 
 (function enterMapPage() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getGeo);
+    navigator.geolocation.getCurrentPosition(getGeo)
+  } else {
+    console.log('error')
   }
-  else {
-    console.log('error');
-  }
-})();
+}())
 
 function getGeo(event) {
   const lat = event.coords.latitude;
@@ -19,13 +18,12 @@ function getGeo(event) {
       center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
       level: 2 // 지도의 확대 레벨
     };
-    getMap(mapOption);
-  }
-  catch (e) {
+    getMap(mapOption)
+  } catch (e) {
     mapErrorModal();
     console.log(e)
   }
-};
+}
 
 const $showMap = document.getElementById('showmap');
 const $mapText = document.querySelector('.mappage__location__text ');
@@ -35,15 +33,13 @@ const pointArr = [];
 function getMap(mapOption) {
   // 지도를 생성합니다
   map = new kakao.maps.Map($showMap, mapOption);
-
   // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
-  searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-
+  searchAddrFromCoords(map.getCenter(), displayCenterInfo)
   // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
   kakao.maps.event.addListener(map, 'idle', function () {
-    searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-  });
-  getBannerData();
+    searchAddrFromCoords(map.getCenter(), displayCenterInfo)
+  })
+  getBannerData()
 }
 
 async function getBannerData() {
@@ -52,17 +48,16 @@ async function getBannerData() {
   try {
     const mapPointResponse = await fetch('../data/mappoint.json');
     const maoPointData = await mapPointResponse.json();
-    pointArr.push(maoPointData.points);
+    pointArr.push(maoPointData.points)
     canLoadBanner = true;
 
-    refreshMyLoaction(canLoadBanner);
-    showBannerInfo(pointArr);
-    setMapPolygon(pointArr);
-    showWalkBanner();
-  }
-  catch (e) {
-    mapErrorModal();
-    console.log(e);
+    refreshMyLoaction(canLoadBanner)
+    showBannerInfo(pointArr)
+    setMapPolygon(pointArr)
+    showWalkBanner()
+  } catch (e) {
+    mapErrorModal()
+    console.log(e)
   }
 }
 
@@ -72,23 +67,23 @@ function mapErrorModal() {
   mapErrorModalDiv.classList.add("mapErrorModal")
 
   const mapErrorModalText = document.createElement('p');
-  mapErrorModalText.classList.add("mapErrorModalText");
-  mapErrorModalText.innerHTML = "데이터를 불러오는데<br/> 실패하였습니다<br/> 재로딩 해주세요";
+  mapErrorModalText.classList.add("mapErrorModalText")
+  mapErrorModalText.innerHTML = "데이터를 불러오는데<br/> 실패하였습니다<br/> 재로딩 해주세요"
 
-  mapErrorModalDiv.append(mapErrorModalText);
-  $mapWrap.append(mapErrorModalDiv);
+  mapErrorModalDiv.append(mapErrorModalText)
+  $mapWrap.append(mapErrorModalDiv)
 }
 
 function refreshMyLoaction(canLoadBanner) {
   if (canLoadBanner === true) {
     getMyLocationData();
     //watchPosition로 하면 너무 부정확해서 setinerval로 보류
-    setInterval(getMyLocationData, 4000);
+    setInterval(getMyLocationData, 4000)
   }
 }
 
 function getMyLocationData() {
-  navigator.geolocation.getCurrentPosition(getMyGeo);
+  navigator.geolocation.getCurrentPosition(getMyGeo)
 }
 //map 오류 고치기
 function getMyGeo(event) {
@@ -105,12 +100,9 @@ function getMyGeo(event) {
 //클릭하면 dragged 가 false로 바뀌고 현재 위치로 바로 갈 수 있게
   $backToMyLoc.addEventListener('click', () => {
     dragged = false;
-    map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
+    map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La))
   })
-};
-
-
-
+}
 
 //내 위치 표시
 const myLocArr = [];
@@ -122,7 +114,7 @@ function showMyLoction(myMapOption) {
   let myLocPosition = new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La);
 
   if (dragged === false) {
-    map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La));
+    map.panTo(new kakao.maps.LatLng(myMapOption.center.Ma, myMapOption.center.La))
   }
   if (myLocArr.length <= 0) {
     currentMyLoc = new kakao.maps.Marker({
@@ -132,9 +124,8 @@ function showMyLoction(myMapOption) {
     });
     myLocArr.push(currentMyLoc);
     currentMyLoc.setMap(map);
-  }
-  else if (myLocArr.length > 0) {
-    currentMyLoc.setPosition(myLocPosition);
+  } else if (myLocArr.length > 0) {
+    currentMyLoc.setPosition(myLocPosition)
   }
 }
 
@@ -147,7 +138,7 @@ const $firstCourseDistance = document.querySelector('.mappage__walkload__course_
 const $firstCourseMoney = document.querySelector('.mappage__walkload__course__money');
 
 const $secondSelectCourse = document.querySelector('.mappage__walkload__course2');
-const $secondCourseImageSecond = document.querySelector('.mappage__walkload__course2__img__img');
+const $secondCourseImage = document.querySelector('.mappage__walkload__course2__img__img');
 const $secondCourseDesc = document.querySelector('.mappage__walkload__course2__desc');
 const $secondCourseName = document.querySelector('.mappage__walkload__course2__name');
 const $secondCourseLocation = document.querySelector('.mappage__walkload__course2__location');
@@ -167,8 +158,8 @@ function showBannerInfo(pointArr) {
       position: markerPos,
       image: markerImg
     });
-    markerArr.push(marker);
-    marker.setMap(map);
+    markerArr.push(marker)
+    marker.setMap(map)
   }
   //배너에 정보 표시
   for (let i = 0; i < markerArr.length; i++) {
@@ -192,17 +183,17 @@ function setMapPolygon(pointArr) {
   for (let i = 0; i < pointArr[0].length; i++) {
     let polygonPath = [];
     for (let j = 0; j < pointArr[0][i].mapPoints.length; j++) {
-      polygonPath.push(new kakao.maps.LatLng(pointArr[0][i].mapPoints[j][0], pointArr[0][i].mapPoints[j][1]));
+      polygonPath.push(new kakao.maps.LatLng(pointArr[0][i].mapPoints[j][0], pointArr[0][i].mapPoints[j][1]))
     }
-    courseArr.push(polygonPath);
+    courseArr.push(polygonPath)
   }
 
   for (let i = 0; i < courseArr.length; i++) {
     polygon = new kakao.maps.Polygon({
       path: courseArr[i], stroke: 5, strokeColor: '#42AB34', strokeOpacity: 1, strokeStyle: 'solid', fillColor: 'none', fillOpacity: 0, 
     });
-    polygon.setMap(map);
-    polygonArr.push(polygon);
+    polygon.setMap(map)
+    polygonArr.push(polygon)
   }
 
   showMapPoint(polygon, polygonArr)
@@ -231,14 +222,14 @@ function showMapPoint(polygon, polygonArr) {
   //첫번째 배너 클릭하면 배너의 코스 보여주고 코스 출발 지점으로 이동
   $firstSelectCourse.addEventListener('click', (event) => {
     let couresFirstLat = Number(event.target.parentNode.offsetParent.childNodes[3].id);
-    map.panTo(new kakao.maps.LatLng(polygonArr[couresFirstLat].Sg[0].Ma, polygonArr[couresFirstLat].Sg[0].La));
+    map.panTo(new kakao.maps.LatLng(polygonArr[couresFirstLat].Sg[0].Ma, polygonArr[couresFirstLat].Sg[0].La))
 
-    for(let i = 1; i <= polygonArr.length; i++) {
-      if(polygon.G.r.childNodes[i].style.cssText) {
+    for (let i = 1; i <= polygonArr.length; i++) {
+      if (polygon.G.r.childNodes[i].style.cssText) {
         polygon.G.r.childNodes[i].style.cssText = "";
       }
     }
-    if(polygonArr[couresFirstLat].Sg[0].Ma === Number(event.path[2].parentElement.firstElementChild.id)) {
+    if (polygonArr[couresFirstLat].Sg[0].Ma === Number(event.path[2].parentElement.firstElementChild.id)) {
       polygon.G.r.childNodes[couresFirstLat+1].style.cssText = `${courseStokeStyle}`;
     }
   })
@@ -248,18 +239,18 @@ function showMapPoint(polygon, polygonArr) {
     let couresFirstLat = Number(event.target.parentNode.offsetParent.childNodes[3].id);
     map.panTo(new kakao.maps.LatLng(polygonArr[couresFirstLat].Sg[0].Ma, polygonArr[couresFirstLat].Sg[0].La));
 
-    for(let i = 1; i <= polygonArr.length; i++) {
-      if(polygon.G.r.childNodes[i].style.cssText) {
+    for (let i = 1; i <= polygonArr.length; i++) {
+      if (polygon.G.r.childNodes[i].style.cssText) {
         polygon.G.r.childNodes[i].style.cssText = "";
       }
     } 
-    if(polygonArr[couresFirstLat].Sg[0].Ma === Number(event.path[2].parentElement.lastElementChild.id)) {
+    if (polygonArr[couresFirstLat].Sg[0].Ma === Number(event.path[2].parentElement.lastElementChild.id)) {
       polygon.G.r.childNodes[couresFirstLat+1].style.cssText = `${courseStokeStyle}`;
     }
   })
   
   //드래그로 지도 이동을 완료했을 때 마지막 파라미터로 넘어온 함수를 호출
-  kakao.maps.event.addListener(map, 'dragend', showWalkBanner);
+  kakao.maps.event.addListener(map, 'dragend', showWalkBanner)
 }
 
 function showWalkBanner() {
@@ -272,12 +263,11 @@ function showWalkBanner() {
 
   pointArr[0].forEach((e, i) => {
     let markToMark = Math.abs((Number(e.lat) + Number(e.lon)) - (mapCenter.Ma + mapCenter.La));
-    if(markToMark < shortDistance) {
+    if (markToMark < shortDistance) {
       shortDistance = markToMark;
       firstNearMark = pointArr[0][i];
-    }
-    else if(markToMark > shortDistance) {
-      if(markToMark < secondShortDistance) {
+    } else if (markToMark > shortDistance) {
+      if (markToMark < secondShortDistance) {
         secondShortDistance = markToMark;
         secondNearMark = pointArr[0][i];
       }
@@ -285,31 +275,42 @@ function showWalkBanner() {
   })
 
   $firstSelectCourse.setAttribute('id', firstNearMark.lat);
-  $firstSourseImage.setAttribute("src", firstNearMark.image)
   $firstCourseDesc.setAttribute('id', firstNearMark.index);
   $firstCourseName.textContent = firstNearMark.name;
   $firstCourseLocation.textContent = firstNearMark.address;
   $firstCourseDistance.textContent = firstNearMark.distance;
   $firstCourseMoney.textContent = firstNearMark.money;
-
   $secondSelectCourse.setAttribute('id', secondNearMark.lat);
-  $secondCourseImageSecond.setAttribute("src", secondNearMark.image)
   $secondCourseDesc.setAttribute('id', secondNearMark.index);
   $secondCourseName.textContent = secondNearMark.name;
   $secondCourseLocation.textContent = secondNearMark.address;
   $secondCourseDistance.textContent = secondNearMark.distance;
   $secondCourseMoney.textContent = secondNearMark.money;
+
+  courseImgLazyload(firstNearMark.image, secondNearMark.image)
 }
 
+function courseImgLazyload(firstImgSrc, secondImgSrc) {
+  const $firstCourseWrap = document.querySelector(".mappage__walkload__course");
+  const $secondCourseWrap = document.querySelector(".mappage__walkload__course2");
+  $firstSourseImage.setAttribute("src", firstImgSrc);
+  $secondCourseImage.setAttribute("src", secondImgSrc);
+  new Promise(resolve => {
+    $firstSourseImage.onload = () => resolve(
+      $firstCourseWrap.classList.add("showContent"),
+      $secondCourseWrap.classList.add("showContent")
+    )
+  })
+}
 
 function searchAddrFromCoords(coords, callback) {
   // 좌표로 행정동 주소 정보를 요청합니다
-  geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+  geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback)
 }
 
 function searchDetailAddrFromCoords(coords, callback) {
   // 좌표로 법정동 상세 주소 정보를 요청합니다
-  geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+  geocoder.coord2Address(coords.getLng(), coords.getLat(), callback)
 }
 
 // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
@@ -324,3 +325,4 @@ function displayCenterInfo(result, status) {
     }
   }
 }
+
