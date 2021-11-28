24143f17375c6d4fe997450c3d66ce8495323370
(yunjin) <sion11290805@gmail.com>
2021-11-28 20:26:08 +0900

    refactor: for문 -> map함수

--- app/static/bankpage_js/bankpage_total.js
+++ app/static/bankpage_js/bankpage_total.js
@@ -1,83 +1,75 @@
 import {setBankTotalData, setBankMoneyGraph, setTitle } from "./bankpage.js";
-const allTitles = ["방구석에 콕", "걷기 비기너", "걷기 주니어", "걷기 프로", "마라톤 선수", ""];
 
 //은행 걷기 데이터
 export function getBankStepData() {
   const getTotalStepData = localStorage.getItem("STEP_DATA");
   const parseTotalStepData = JSON.parse(getTotalStepData);
-
   let totalStepData = _.go(
     parseTotalStepData.steps_count,
     _.map(data => data.value),
     _.reduce(_.add)
   );
 
-  setBankTotalData(totalStepData);
-  setBankData(totalStepData);
+  setBankTotalData(totalStepData)
+  setBankData(totalStepData)
 }
 
 //은행 돈 그래프 
 export function setBankData(totalStepData) {
+  const allTitles = ["방구석에 콕", "걷기 비기너", "걷기 주니어", "걷기 프로", "마라톤 선수", ""];
   let currentTitleNum;
   let bankTotalPrice = totalStepData*5;
   let bankMoneyGraghMax;
   let bankMoneyGraghMin;
-
   if(bankTotalPrice >= 0 &&  bankTotalPrice <= 100000) {
     bankMoneyGraghMax = 100000;
     bankMoneyGraghMin = 0;
     currentTitleNum = 0;
-  }
-  else if(bankTotalPrice > 100000 &&  bankTotalPrice <= 500000) {
+  } else if(bankTotalPrice > 100000 &&  bankTotalPrice <= 500000) {
     bankMoneyGraghMax = 500000;
     bankMoneyGraghMin = 100000;
     currentTitleNum = 1;
-  }
-  else if(bankTotalPrice > 500000 &&  bankTotalPrice <= 1000000) {
+  } else if(bankTotalPrice > 500000 &&  bankTotalPrice <= 1000000) {
     bankMoneyGraghMax = 1000000;
     bankMoneyGraghMin = 500000;
     currentTitleNum = 2;
-  }
-  else if(bankTotalPrice > 1000000 &&  bankTotalPrice <= 3000000) {
+  } else if(bankTotalPrice > 1000000 &&  bankTotalPrice <= 3000000) {
     bankMoneyGraghMax = 3000000;
     bankMoneyGraghMin = 1000000;
     currentTitleNum = 3;
-  }
-  else if(bankTotalPrice > 3000000 &&  bankTotalPrice <= 10000000) {
+  } else if(bankTotalPrice > 3000000 &&  bankTotalPrice <= 10000000) {
     bankMoneyGraghMax= 10000000;
     bankMoneyGraghMin = 3000000;
     currentTitleNum = 4;
   }
   
-  setBankMoneyGraph(bankTotalPrice, bankMoneyGraghMax, bankMoneyGraghMin);
-  localStorage.setItem("USER_TITLE", JSON.stringify(allTitles[currentTitleNum-1]));
-  setTitle(currentTitleNum, allTitles);
+  localStorage.setItem("USER_TITLE", JSON.stringify(allTitles[currentTitleNum - 1]))
+  setBankMoneyGraph(bankTotalPrice, bankMoneyGraghMax, bankMoneyGraghMin)
+  setTitle(currentTitleNum, allTitles)
 }
 
 //칭모 모달 생성
 export function titleModal(e) {
+  const allTitles = ["방구석에 콕", "걷기 비기너", "걷기 주니어", "걷기 프로", "마라톤 선수", ""];
   const titleModaleBox = document.createElement('div');
-  titleModaleBox.classList.add("titleModal");
-
   const titleModalTitle = document.createElement("h2");
-  titleModalTitle.textContent = "모든 칭호";
-  titleModalTitle.classList.add("titleModalTitle");
-
   const titleModalClose = document.createElement('button');
+  titleModalTitle.textContent = "모든 칭호";
   titleModalClose.textContent = "X";
   titleModalClose.classList.add("titleModalClose");
+  titleModaleBox.classList.add("titleModal");
+  titleModalTitle.classList.add("titleModalTitle");
   titleModalClose.addEventListener('click', () => {
-    titleModaleBox.remove();
-  });
-  titleModaleBox.append(titleModalClose);
-  titleModaleBox.append(titleModalTitle);
-
-  for(let title of allTitles){
-    let titles = document.createElement('p');
+    titleModaleBox.remove()
+  })
+  titleModaleBox.append(titleModalClose)
+  titleModaleBox.append(titleModalTitle)
+  _.map(title => {
+    const titles = document.createElement('p');
     titles.classList.add("titles")
     titles.textContent = title;
     titleModaleBox.append(titles);
-  }
+  }, allTitles)
 
   e.target.parentNode.parentNode.parentNode.parentNode.append(titleModaleBox);
 }
