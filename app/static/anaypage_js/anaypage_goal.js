6fa17902cf9dcb1365c21c5fa61ef2dbd241be81
(yunjin) <sion11290805@gmail.com>
2021-11-28 20:16:20 +0900

    refactor: for문 -> for of문, 함수형코드

--- app/static/anaypage_js/anaypage_goal.js
+++ app/static/anaypage_js/anaypage_goal.js
@@ -4,50 +4,15 @@ import { chartDataArr } from './anaypage_step.js';
 export function setGoalAchieve(goalBoxArr, goalWeekNum) {
   const goalStep = localStorage.getItem("STEP_GOAL");
   const noGoalText = `<div class="noGoalText">목표를 설정해주세요</div>`;
-
-  if(!goalWeekNum) goalWeekNum = 0;
-  
-  if(goalStep) {
-
+  if (!goalWeekNum) goalWeekNum = 0;
+  if (goalStep) {
     _.map(a => a.classList.remove("acheiveGoal", "NotAcheiveGoal"), goalBoxArr);
-
-    const chartDataOverGoalStepArr = _.go(
-      chartDataArr[goalWeekNum],
-      _.map(chartData => chartData.value >= goalStep)
-    )
-
-    // for(let i = 0;  i < goalBoxArr.length; i++) {
-    //   if(chartDataArr[goalWeekNum][i].value >= goalStep) {
-    //     goalBoxArr[i].classList.add("acheiveGoal")
-    //   }
-    // }
-
-    // for(let i = 0;  i < goalBoxArr.length; i++) {
-    //   if(chartDataArr[goalWeekNum][i].value < goalStep) {
-    //     goalBoxArr[i].classList.add("NotAcheiveGoal")
-    //   }
-    // }
-
-    console.log(goalBoxArr)
-
-    // _.go(
-    //   goalBoxArr,
-    //   _.map(goalBox => _.go(
-    //     chartDataArr[goalWeekNum],
-    //     _.map(chartData => )
-    //   ))
-    // )
-
-    for(let i = 0; i < goalBoxArr.length; i++) {
-      {chartDataArr[goalWeekNum][i].value >= goalStep
-        ? goalBoxArr[i].classList.add("acheiveGoal")
-        :goalBoxArr[i].classList.add("NotAcheiveGoal")}
+    for (const [setData, goalDiv] of _.zip(chartDataArr[goalWeekNum], goalBoxArr)) {
+      setData.value >= Number(goalStep) 
+      ? goalDiv.classList.add("acheiveGoal")
+      : goalDiv.classList.add("NotAcheiveGoal");
     }
-
-
-  }
-  else {
+  } else {
     return noGoalText;
   }
-
 }
