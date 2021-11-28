a0243fbff376c144ad830e5bc1eed93b00b64ab8
(yunjin) <sion11290805@gmail.com>
2021-11-28 20:29:14 +0900

    refector: 변수명 변경

--- app/static/anaypage_js/anaypage_goal.js
+++ app/static/anaypage_js/anaypage_goal.js
@@ -6,7 +6,7 @@ export function setGoalAchieve(goalBoxArr, goalWeekNum) {
   const noGoalText = `<div class="noGoalText">목표를 설정해주세요</div>`;
   if (!goalWeekNum) goalWeekNum = 0;
   if (goalStep) {
-    _.map(a => a.classList.remove("acheiveGoal", "NotAcheiveGoal"), goalBoxArr);
+    _.map(goalDiv => goalDiv.classList.remove("acheiveGoal", "NotAcheiveGoal"), goalBoxArr);
     for (const [setData, goalDiv] of _.zip(chartDataArr[goalWeekNum], goalBoxArr)) {
       setData.value >= Number(goalStep) 
       ? goalDiv.classList.add("acheiveGoal")
