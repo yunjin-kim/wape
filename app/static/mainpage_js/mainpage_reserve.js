483e6f71d1798ca6a50b6badbc3a7814398216f1
(yunjin) <sion11290805@gmail.com>
2021-12-19 21:28:39 +0900

    refactor: 예약기능 전역 변수 제거

--- app/static/mainpage_js/mainpage_reserve.js
+++ app/static/mainpage_js/mainpage_reserve.js
@@ -1,7 +1,5 @@
 import { showTimeOptionAtReserve } from './mainpage.js';
 
-const date = new Date();
-
 //언제 걸을까요 클릭하면 background색
 const CLICK_GREEN = 'backgroundGreen';
 const DATE_SPAN = 'dateSpan';
@@ -11,17 +9,13 @@ export function clickDate(e) {
   if(e.target.classList.contains(COLORED_BOX)) {
     if(e.target.classList.contains(CLICK_GREEN)) {
       e.target.classList.remove(CLICK_GREEN);
-    }
-    else {
+    } else {
       e.target.classList.add(CLICK_GREEN);
     }
-  }
-
-  else if(e.target.classList.contains(DATE_SPAN)) {
+  } else if(e.target.classList.contains(DATE_SPAN)) {
     if(e.target.parentNode.classList.contains(CLICK_GREEN)) {
       e.target.parentNode.classList.remove(CLICK_GREEN);
-    }
-    else {
+    } else {
       e.target.parentNode.classList.add(CLICK_GREEN);
     }
   }
@@ -31,20 +25,18 @@ export function setTimeOptionAtReserve() {
   const hourArr = [];
   const minuteArr = [];
   
-  //몇시에 걸을까요 시 option값
-  for(let i = 1; i < 24; i++) {
-    if(i < 10){
-      i = "0"+i;
+  for( let i = 1; i < 24; i++) { //몇시에 걸을까요 시 option값
+    if (i < 10){
+      i = "0" + i;
     }
     const option = document.createElement('option');
     option.textContent = i;
     hourArr.push(option);
-    }
+  }
   
-  //몇시에 걸을까요 분 option값
-  for(let i = 1; i < 60; i++){
-    if(i < 10){
-      i = "0"+i;
+  for (let i = 1; i < 60; i++) { //몇시에 걸을까요 분 option값
+    if (i < 10){
+      i = "0" + i;
     }
     const option = document.createElement('option');
     option.textContent = i;
@@ -56,44 +48,32 @@ export function setTimeOptionAtReserve() {
 //걷기 알림 날짜 클릭
 let reserveArr = []; 
 export function setClickDateArr(e) {
-  if(e.target.classList.contains(COLORED_BOX)) {
+  if (e.target.classList.contains(COLORED_BOX)) {
     //박스가 초록색이라면
-    if(e.target.classList.contains(CLICK_GREEN)) {
+    if (e.target.classList.contains(CLICK_GREEN)) {
       reserveArr.filter((date, idx) => {
         date === e.target.children[1].textContent ? reserveArr.splice(idx, 1) : " ";
       });
-    }
-    //박스가 초록색이 아닐마면
-    else {
+    } else { //박스가 초록색이 아닐마면
       reserveArr.push(e.target.children[1].textContent);
     };
-  }
-  // span 클릭하면 
-  else if(e.target.classList.contains(DATE_SPAN)) {
-    //부모 박스가 초록색이면
-    if(e.target.parentNode.classList.contains(CLICK_GREEN)) {
-      //요일 클릭
-      if(e.target.parentNode.children[0]) {
+  } else if (e.target.classList.contains(DATE_SPAN)) { // span 클릭하면 
+    if (e.target.parentNode.classList.contains(CLICK_GREEN)) { //부모 박스가 초록색이면
+      if (e.target.parentNode.children[0]) { //요일 클릭
         e.target.parentNode.children[1].textContent;
         reserveArr.filter((date, idx) => {
           date === e.target.parentNode.children[1].textContent ? reserveArr.splice(idx, 1) : " ";
         });
-      }
-      //날짜 클릭
-      else {
+      } else { //날짜 클릭
         reserveArr.filter((date, idx) => {
           date === e.target.textContent ? reserveArr.splice(idx, 1) : " ";
         });
       }
-    }
-    //부모 박스가 초록색이 아니라면
-    else {
+    } else { //부모 박스가 초록색이 아니라면
       //요일 클릭
       if(e.target.parentNode.children[0]) {
         reserveArr.push(e.target.parentNode.children[1].textContent);
-      }
-      //날짜 클릭
-      else {
+      } else { //날짜 클릭
         reserveArr.push(e.target.textContent);
       }
     }
@@ -102,20 +82,19 @@ export function setClickDateArr(e) {
 
 //이전 걷기 예약 제거
 export function beforeReseveDelete() {
+  const date = new Date();
   const getReserveDate = localStorage.getItem("RESERVE_DATE");
   const parseGetReserveDate = JSON.parse(getReserveDate);
 
-  if(parseGetReserveDate) {
-    //오늘 이전 예약 
-    let subLastReserve = _.filter(
+  if (parseGetReserveDate) {
+    let subLastReserve = _.filter( //오늘 이전 예약 
       d => d.date < date.getDate(), parseGetReserveDate
     )
-    if(subLastReserve) {
-      //오늘 포함 이후 예약
-      let afterReserve = _.filter(
+    if (subLastReserve) {
+      let afterReserve = _.filter( //오늘 포함 이후 예약
         d => d.date >= date.getDate(), parseGetReserveDate
       )
-      if(subLastReserve.length > 0) {
+      if (subLastReserve.length > 0) {
           localStorage.setItem("RESERVE_DATE",JSON.stringify(afterReserve));
       }
     }
@@ -125,8 +104,10 @@ export function beforeReseveDelete() {
 //예약 기능
 export function clickReserve(reserveHour, reserveMinute) {
   const reserveObjArr = [];
+  const getReserveDate = localStorage.getItem("RESERVE_DATE");
+  let parseGetReserveDate = JSON.parse(getReserveDate);
 
-  for(let reserveDate of reserveArr) {
+  for (let reserveDate of reserveArr) {
     let reserveObj = {
       date: reserveDate,
       hour: reserveHour,
@@ -136,13 +117,9 @@ export function clickReserve(reserveHour, reserveMinute) {
   }
   reserveArr = [];
 
-  let getReserveDate = localStorage.getItem("RESERVE_DATE");
-  let parseGetReserveDate = JSON.parse(getReserveDate);
-  console.log(parseGetReserveDate)
-  if(parseGetReserveDate) {
+  if (parseGetReserveDate) {
     localStorage.setItem("RESERVE_DATE",JSON.stringify(parseGetReserveDate.concat(reserveObjArr)));
-  }
-  else {
+  } else {
     localStorage.setItem("RESERVE_DATE",JSON.stringify(reserveObjArr));
   }
 }
