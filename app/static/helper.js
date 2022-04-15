687514e4382cd05e439276dc8e27ac5d3d7399fa
(yunjin) <sion11290805@gmail.com>
2022-04-15 15:35:58 +0900

    refactor: helper 함수 수정

--- app/static/helper.js
+++ app/static/helper.js
@@ -1,71 +1,28 @@
-export function qs(selector, scope = document) {
-  if (!selector) throw "no selector";
-
+function qs(selector, scope = document) {
   return scope.querySelector(selector);
 }
 
-export function qsAll(selector, scope = document) {
-  if (!selector) throw "no selector";
-
+function qsAll(selector, scope = document) {
   return Array.from(scope.querySelectorAll(selector));
 }
 
-export function on(target, eventName, handler) {
-  target.addEventListener(eventName, handler);
-}
+const addEvent = (target, eventName, handler) => {
+  target.addEventListener(eventName, (event) => {
+    try {
+      handler(event);
+    } catch (error) {
+      return console.log(error);
+    }
+  });
+};
 
-export function creatEl(element, scope = document) {
-  if (!element) throw "no element";
-
+function creatEl(element, scope = document) {
   return scope.createElement(element);
 }
 
-export function delegate(target, eventName, selector, handler) {
-  const emitEvent = (event) => {
-    const potentialElements = qsAll(selector, target);
-
-    for (const potentialElement of potentialElements) {
-      if (potentialElement === event.target) {
-        return handler.call(event.target, event);
-      }
-    }
-  };
-
-  on(target, eventName, emitEvent);
-}
-
-export function emit(target, eventName, detail) {
+function customEv(target, eventName, detail) {
   const event = new CustomEvent(eventName, { detail });
   target.dispatchEvent(event);
 }
 
-export function formatRelativeDate(date = new Date()) {
-  const TEN_SECOND = 10 * 1000;
-  const A_MINUTE = 60 * 1000;
-  const A_HOUR = 60 * A_MINUTE;
-  const A_DAY = 24 * A_HOUR;
-
-  const diff = new Date() - date;
-
-  if (diff < TEN_SECOND) return `방금 전`;
-  if (diff < A_MINUTE) return `${Math.floor(diff / 1000)}초 전`;
-  if (diff < A_HOUR) return `${Math.floor(diff / 1000 / 60)}분 전`;
-  if (diff < A_DAY) return `${Math.floor(diff / 1000 / 60 / 24)}시간 전`;
-  return date.toLocaleString("ko-KR", {
-    hour12: false,
-    dateStyle: "medium",
-  });
-}
-
-export function createPastDate(date = 1, now = new Date()) {
-  if (date < 1) throw "date는 1 이상입니다";
-
-  const yesterday = new Date(now.setDate(now.getDate() - 1));
-  if (date === 1) return yesterday;
-
-  return createPastDate(date - 1, yesterday);
-}
-
-export function createNextId(list = []) {
-  return Math.max(...list.map((item) => item.id)) + 1;
-}
+export { qs, qsAll, addEvent, creatEl, customEv };
