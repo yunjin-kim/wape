398e04e76e9693b808b851d66d7fa9f9b517c1bc
(yunjin) <sion11290805@gmail.com>
2022-12-29 00:41:51 +0900

    style: theme color 잘못된 값 수정

--- src/client/styles/colors.ts
+++ src/client/styles/colors.ts
@@ -1,19 +1,20 @@
 export const colors = {
-  BLACK_900: '#00000',
+  BLACK_900: '#000000',
   BLACK_700: '#191919',
+  BLACK_OPACITY_200: 'rgba(0, 0, 0, 0.2)',
 
   GRAY_200: '#F8F8FA',
   GRAY_300: '#F1F1F5',
   GRAY_400: '#EDEDED',
   GRAY_500: '#DBDBDB',
-  GRAY_700: '#99999',
+  GRAY_700: '#999999',
   GRAY_900: '#707070',
 
   GREEN_700: '#42AB34',
 
   YELLOW_700: '#E5C84D',
 
-  WHITE_000: '#FFFFF',
+  WHITE_000: '#FFFFFF',
 } as const;
 
 export type Colors = typeof colors;
