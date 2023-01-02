b38a8ba71420aee8cd81d4c3835a07448d2a43b1
(yunjin) <sion11290805@gmail.com>
2023-01-02 16:36:05 +0900

    feat: Text 컴포넌트 props className 추가
    emotion에서 스타일 컴포넌트 합성을 위해서는 className을 합성당하는 컴포넌트에 추가 해야 함

--- src/client/components/@common/Text.tsx
+++ src/client/components/@common/Text.tsx
@@ -8,11 +8,18 @@ interface Props extends PropsWithChildren {
   size?: number;
   align?: 'center' | 'left' | 'right' | 'initial';
   color?: ColorsValue;
+  className?: string;
 }
 
-const Text = ({ size = 1, align = 'initial', color = theme.colors.BLACK_700, children }: Props) => {
+const Text = ({
+  size = 1,
+  align = 'initial',
+  color = theme.colors.BLACK_700,
+  className,
+  children,
+}: Props) => {
   return (
-    <S.Container size={size} align={align} color={color}>
+    <S.Container className={className} size={size} align={align} color={color}>
       {children}
     </S.Container>
   );
