a74fddd3fe5ac5fee4adc6537a18b8f49816b3e1
(yunjin) <sion11290805@gmail.com>
2023-01-07 18:46:41 +0900

    feat: Text 컴포넌트 props 수정

--- src/client/components/@common/Text.tsx
+++ src/client/components/@common/Text.tsx
@@ -8,18 +8,17 @@ interface Props extends PropsWithChildren {
   size?: number;
   align?: 'center' | 'left' | 'right' | 'initial';
   color?: ColorsValue;
-  className?: string;
 }
 
 const Text = ({
   size = 1,
   align = 'initial',
   color = theme.colors.BLACK_700,
-  className,
   children,
+  ...props
 }: Props) => {
   return (
-    <S.Container className={className} size={size} align={align} color={color}>
+    <S.Container size={size} align={align} color={color} {...props}>
       {children}
     </S.Container>
   );
